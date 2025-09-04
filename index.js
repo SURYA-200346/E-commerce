0185bf72c6c8865b388b91aacd303d02ae666b4a
SURYA-200346 <surya.jsdev@gmail.com>
2025-09-04 20:53:52 +0530

    login form fetch data added

---
+++ index.js
@@ -0,0 +1,57 @@
+
+
+const Loginbtn = document.getElementById("Loginbtn");
+const logincontainer = document.querySelector(".login-container");
+const close = document.getElementById("close");
+const success = document.getElementById("success");
+Loginbtn.addEventListener("click", () => {
+    logincontainer.style.display = "block";
+})
+close.addEventListener("click", () => {
+    logincontainer.style.display = "none";
+})
+window.addEventListener("click", (close) => {
+    if (close.target === logincontainer) {
+        logincontainer.style.display = "none";
+    }
+});
+
+document.getElementById("login").addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    fetch("https://68a582352a3deed2960dbd2c.mockapi.io/form/name", {
+        method: "GET",
+        headers: { "content-type": "application/json" }
+    })
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error("Failed to fetch users");
+        })
+        .then(users => {
+            const user = users.find(
+                u => (u.username === username || u.name === username) && (u.newpass === password || u.password === password)
+            );
+            if (user) {
+                document.getElementById("success").innerText = "Login successful!";
+                document.getElementById("failed").textContent = "";
+                logincontainer.style.display = "none";
+                alert("Login Successfull")
+            } else {
+                document.getElementById("failed").textContent = "Invalid username or password.";
+                document.getElementById("success").textContent = "";
+                logincontainer.style.display = "none";
+                alert("Invalid username or password.")
+            }
+            document.getElementById("loginform").reset();
+
+        })
+        .catch(error => {
+            document.getElementById("failed").textContent = "Login failed. Please try again.";
+            document.getElementById("success").textContent = "";
+        });
+});
