6ea989eb9182c209659cd5f4eb2ae3fd63872f0f
SURYA-200346 <surya.jsdev@gmail.com>
2025-09-09 13:35:12 +0530

    sign up logic added

--- index.js
+++ index.js
@@ -1,20 +1,42 @@
 
 
 const Loginbtn = document.getElementById("Loginbtn");
+const signbtn = document.getElementById("signbtn");
 const logincontainer = document.querySelector(".login-container");
 const close = document.getElementById("close");
 const success = document.getElementById("success");
+const signupcontainer = document.getElementById("signupcontainer")
+
 Loginbtn.addEventListener("click", () => {
     logincontainer.style.display = "block";
 })
 close.addEventListener("click", () => {
     logincontainer.style.display = "none";
+
 })
+// close.addEventListener("click", () => {
+//     signupcontainer.style.display="none";
+
+// })
 window.addEventListener("click", (close) => {
     if (close.target === logincontainer) {
         logincontainer.style.display = "none";
     }
 });
+window.addEventListener("click", (close) => {
+    if (close.target === signupcontainer) {
+        signupcontainer.style.display = "none";
+    }
+});
+Loginbtn.addEventListener("click", () => {
+    signupcontainer.style.display = "none"
+})
+signbtn.addEventListener("click", () => {
+    logincontainer.style.display = "none"
+})
+signbtn.addEventListener("click", () => {
+    signupcontainer.style.display = "block"
+})
 
 document.getElementById("login").addEventListener("click", (event) => {
     event.preventDefault();
@@ -34,10 +56,11 @@ document.getElementById("login").addEventListener("click", (event) => {
         })
         .then(users => {
             const user = users.find(
-                u => (u.username === username || u.name === username) && (u.newpass === password || u.password === password)
+                u => (u.username === username || u.name === username) && (u.newpassword === password || u.newpassword === password)
             );
             if (user) {
                 document.getElementById("success").innerText = "Login successful!";
+                document.getElementById("success").style.display="none"
                 document.getElementById("failed").textContent = "";
                 logincontainer.style.display = "none";
                 alert("Login Successfull")
@@ -55,3 +78,35 @@ document.getElementById("login").addEventListener("click", (event) => {
             document.getElementById("success").textContent = "";
         });
 });
+
+
+
+document.getElementById("signupbtn").addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const newpassword = document.getElementById("newpassword").value;
+
+
+    fetch("https://68a582352a3deed2960dbd2c.mockapi.io/form/name", {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify({ name, email, newpassword }),
+
+    })
+        .then(res => {
+            if (res.ok) {
+                return res.json();
+            }
+            throw new Error("Failed to create task.");
+        })
+        .then(data => {
+            alert("Signup successful!");
+        })
+        .catch(error => {
+            console.log("Signup to Create", error);
+
+        })
+    signup.reset();
+})
